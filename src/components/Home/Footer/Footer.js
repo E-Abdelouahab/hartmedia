9056175c6a10eabcc97210683ea1b3f447d5f656
E-Abdelouahab <abdelouahabelhassnaoui36@gmail.com>
2022-03-07 02:52:08 +0100

    agency project react

---
+++ src/components/Home/Footer/Footer.js
@@ -0,0 +1,129 @@
+import React from 'react'
+import logo from '../../../images/logo.png'
+import sm1 from '../../../images/blog/small/1.jpg'
+import sm2 from '../../../images/blog/small/2.jpg'
+import sm3 from '../../../images/blog/small/3.jpg'
+
+function Footer() {
+    return (
+        <div id="rs-footer" class="rs-footer style4">
+            <div class="" style={{width: '90%', margin: 'auto'}}>
+                <div class="footer-newsletter">
+                    <div class="row y-middle">
+                        <div class="col-md-6 sm-mb-26">
+                            <h3 class="title white-color mb-0">Newsletter Subscribe</h3>
+                        </div>
+                        <div class="col-md-6 text-right">
+                            <form class="newsletter-form">
+                                <input type="email" name="email" placeholder="Your email address" required=""/>
+                                <button type="submit"><i class="fa fa-paper-plane"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-content pt-62 pb-79 md-pb-64 sm-pt-48">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-12 col-sm-12 footer-widget md-mb-39">
+                            <div class="about-widget pr-15">
+                                <div class="logo-part">
+                                    <a href="index.html"><img src={logo} alt="Footer Logo"/></a>
+                                </div>
+                                <p class="desc">Heart Media Technology is a young dynamic company with an innovative concept in the field of online file management and planning for flexible workers. Through years of experience, including as a planner in the audiovisual industry, programming, applications, and web design.</p>
+                                <div class="btn-part">
+                                    <a class="readon" href="about.html">Discover More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-12 col-sm-12 md-mb-32 footer-widget">
+                            <h4 class="widget-title">Contact Info</h4>
+                            <ul class="address-widget pr-40">
+                                <li>
+                                    <i class="flaticon-location"></i>
+                                    <div class="desc">Lot 660, Ben Guerir 43150, Morocco.</div>
+                                </li>
+                                <li>
+                                    <i class="flaticon-call"></i>
+                                    <div class="desc">
+                                        <a href="tel:+212697405100">(+212)697405100</a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <i class="flaticon-email"></i>
+                                    <div class="desc">
+                                        <a href="mailto:abdelouahabelhassnaoui36@gmail.com">abdelouahabelhassnaoui36@gmail.com</a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <i class="flaticon-clock"></i>
+                                    <div class="desc">
+                                        09:00 - 18:00
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-4 col-md-12 col-sm-12 footer-widget">
+                            <h4 class="widget-title">Latest Posts</h4>
+                            <div class="footer-post">
+                                <div class="post-wrap mb-15">
+                                    <div class="post-img">
+                                        <a href="blog-single.html"><img src={sm1} alt=""/></a>
+                                    </div>
+                                    <div class="post-desc">
+                                        <a href="blog-single.html">Covid-19 threatens the next generation of smartphones</a>
+                                        <div class="date-post">
+                                            <i class="fa fa-calendar"></i>
+                                            Mars 6, 2022
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="post-wrap mb-15">
+                                    <div class="post-img">
+                                        <a href="blog-single.html"><img src={sm2} alt=""/></a>
+                                    </div>
+                                    <div class="post-desc">
+                                        <a href="blog-single.html">Soundtrack filma Lady Exclusive Music</a>
+                                        <div class="date-post">
+                                            <i class="fa fa-calendar"></i>
+                                            Mars 6, 2022
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="post-wrap">
+                                    <div class="post-img">
+                                        <a href="blog-single.html"><img src={sm3} alt=""/></a>
+                                    </div>
+                                    <div class="post-desc">
+                                        <a href="blog-single.html">Winged moved stars, fruit creature seed night.</a>
+                                        <div class="date-post">
+                                            <i class="fa fa-calendar"></i>
+                                            Mars 6, 2022
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-bottom">
+                    <div class="row y-middle">
+                        <div class="col-lg-6 col-md-8 sm-mb-21">
+                            <div class="copyright">
+                                <p>© Copyright 2021 Hart Media Technology. All Rights Reserved.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-4 text-right sm-text-center">
+                            <ul class="footer-social">
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
